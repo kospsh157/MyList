8193579d6c1dbe98ab311a92238ec53ffa36e73d
SungHo Park <kospsh157@gmail.com>
2022-04-23 11:44:47 -0700

    Added git url

--- app.js
+++ app.js
@@ -11,16 +11,22 @@ class App {
     this.dialogRoot = dialogRoot;
     this.page = new Page(ItemPageComponent);
     this.page.attachTo(appRoot);
+
+    const gitNoteP = document.createElement("p");
+    gitNoteP.innerHTML = `<p>
+      You can check codes of this project at <a href='https://github.com/kospsh157/MyList'>https://github.com/kospsh157/MyList</a>
+    </p>`;
+    const gitNote = new Note("Welcome My Study Project.", gitNoteP);
+    this.page.addChild(gitNote);
+
     const img = new Image(
       "You can save image, youtube, note, todo on here.",
       "https://picsum.photos/800/400"
     );
     this.page.addChild(img);
-    const yout = new Youtube(
-      "The important thing is to prepare the resources and time to get back up from failure.",
-      "https://youtu.be/3KvdWd9iqAY"
-    );
+    const yout = new Youtube("Youtube Video", "https://youtu.be/7aEUakKMLBs");
     this.page.addChild(yout);
+
     const pForNote1 = document.createElement("p");
     pForNote1.innerHTML = `
       If you want add a image, click image button and type 'https://picsum.photos/500/250' at URL. <br>
@@ -28,6 +34,7 @@ class App {
     `;
     const note1 = new Note("MyList Usage (Add Image)", pForNote1);
     this.page.addChild(note1);
+
     const pForNote2 = document.createElement("p");
     pForNote2.innerHTML = `<p>
       If you want add Youtube video, click video button and type youtube url you want at URL.<br>
@@ -90,7 +97,7 @@ new App(document.querySelector(".document"), document.body);
     1. 따라서 그냥 이미 빌드된 js파일을 루트폴더에 만들어놔야 한다.
   3. dist 폴더가 있으면 오히려 js파일을 불러오지 못 한다. 
     1. dist 폴더에 있던 js파일을 루트폴더로 옮기니깐 정상적으로 서버가 js파일을 읽을 수 있었다.
-      1. 그 원인을 찾았다. .gitignore 파일에 dist는 제외되어서 애초에 깃레포짓에 dist폴더를 업로드 되지 않았던 것이다.
+      1. 그 원인을 찾았다. .gitignore 파일에 dist는 제외되어서 애초에 깃레포짓에 dist폴더가 업로드 되지 않았던 것이다.
   4. aws 서버는 자동으로 index.html 파일을 찾아 호스팅하려고 한다. 
   5. 백엔드, 프론트엔드 부분에서 자동으로 빌드를 하려고 한다. 
 */
